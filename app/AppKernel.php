7f94a22f21229fde318e2ca75556551f86c4d690
Omar Yepez <oyepez003@gmail.com>
2014-01-30 16:30:53 -0430

    [new] New bundles added in the Kernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-			new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new Yepsua\MenuBundle\YepsuaMenuBundle(),
             new Yepsua\SmarTwigBundle\YepsuaSmarTwigBundle(),
             new Yepsua\CommonsBundle\YepsuaCommonsBundle(),
